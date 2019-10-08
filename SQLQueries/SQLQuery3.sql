a1882270e5d2bfff23d578cd0556011a3b60702f
Javon Negahban <javon.negahban@cox.net>
2019-10-08 12:16:24 -0500

    Completed fourth question in SQL

--- SQLQueries/SQLQuery3.sql
+++ SQLQueries/SQLQuery3.sql
@@ -16,6 +16,8 @@ FROM Employee
 WHERE Title LIKE '%Sales%';
 
 -- 4. show a list of all countries in billing addresses on invoices.
+SELECT DISTINCT BillingCountry
+FROM Invoice;
 
 -- 5. how many invoices were there in 2009, and what was the sales total for that year?
 --    (extra challenge: find the invoice count sales total for every year, using one query)
